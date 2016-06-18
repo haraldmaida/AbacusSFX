bd614ae3fe1ab58f2a16686d7691a07a8063ac2b
Harald Maida <harald.maida@innoave.com>
2016-06-19 01:25:00 +0200

    started with main app and basic model

---
+++ src/main/scala/com/innoave/abacus/app/FxUiApp.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, Innoave.com
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL INNOAVE.COM OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.innoave.abacus.app
+
+import com.innoave.abacus.model.DefaultParameter
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.scene.shape.Circle
+import com.innoave.abacus.view.BallView
+import com.innoave.abacus.model.Ball
+
+object FxUiApp extends JFXApp {
+
+  implicit val params = DefaultParameter
+
+  stage = new PrimaryStage {
+    title = "Abacus SFX"
+    width = params.boardWidth
+    height = params.boardHeight
+
+    scene = new Scene {
+      content = Seq(new BallView(new Ball()) {
+        centerX = params.ballRadius
+        centerY = params.ballRadius
+      })
+    }
+  }
+
+}
---
+++ src/main/scala/com/innoave/abacus/model/Parameter.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016, Innoave.com
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL INNOAVE.COM OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.innoave.abacus.model
+
+trait Parameter {
+
+  def radix: Int
+  def ballRadius: Int
+  def moveAway: Int
+
+  final def ballDiameter = 2 * ballRadius
+
+  def boardWidth = radix * ballDiameter
+  def boardHeight = radix * ballDiameter
+
+}
+
+trait DefaultParameter extends Parameter {
+
+  override val radix = 10
+
+  override val ballRadius = 25
+
+  val moveAway = radix * ballDiameter
+
+}
+
+object DefaultParameter extends DefaultParameter
